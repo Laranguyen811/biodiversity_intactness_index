d2bf0af5e85736377f1e919d6b7b7249085c8c01
lara_nguyen <laranguyen811@gmail.com>
2025-10-27 16:01:59 +1100

    Added data and updated bio intactness index.

--- bio_intact_index.R
+++ bio_intact_index.R
@@ -1,11 +1,7 @@
-bbi_data <- read.csv("C:/Users/User/Documents/Datasets/biodiversity_intactness_index_nhm\resource.csv")
-getwd() # Check the current working directory
-
-setwd("C:/Users/User/Documents/Datasets/biodiversity_intactness_index_nhm") # Set the working directory
 
 library(dplyr) # A powerful package for efficient and intuitive data manipulation
 
-bbi_data <- read.csv("resource.csv") # Load the dataset
+bbi_data <- read.csv("data/resource.csv") # Load the dataset
 
 head(bbi_data) # Display the first few rows of the dataset
 
@@ -15,3 +11,6 @@ str(bbi_data$area_code) # Check the structure of the area_code column
 
 result <- bbi_data %>% filter( grepl("-AUS$", area_code)) # Filter rows where area_code contains "AUS"
 print(result)
+# Ordering result by years in ascending order
+result_ordered_asc_yr <- result %>% arrange(year)
+print(result_ordered_asc_yr)
